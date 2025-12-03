34f8de3f459cffed19b5844bf292f5dfb878acb9
mfoniso <mfonisoischris@gmail.com>
2025-12-03 23:43:45 +0100

    fixes: bug fixes

--- database/migrations/2024_10_11_202458_change_purchased_customer_attendee.php
+++ database/migrations/2024_10_11_202458_change_purchased_customer_attendee.php
@@ -4,16 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('purchased_tickets', function (Blueprint $table) {
-            $table->dropForeign(['customer_id']);
-            $table->dropColumn('customer_id');
             $table->unsignedBigInteger('attendee_id');
             $table->foreign('attendee_id')
                 ->references('id')
