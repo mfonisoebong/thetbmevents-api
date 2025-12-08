fc9057623b3109227d1a4f2b7d945d593a837668
mfoniso <mfonisoischris@gmail.com>
2025-12-08 08:11:35 +0100

    fixes: made latitude and longitude fields required is event type is physical

--- app/Http/Requests/UpdateEventRequest.php
+++ app/Http/Requests/UpdateEventRequest.php
@@ -38,8 +38,8 @@ class UpdateEventRequest extends FormRequest
             'categories' => ['required'],
             'tickets.*' => ['required'],
             'tickets' => ['array', 'required'],
-            'longitude' => ['required', 'numeric'],
-            'latitude' => ['required', 'numeric'],
+            'longitude' => ['required_if:type,physical', 'numeric'],
+            'latitude' => ['required_if:type,physical', 'numeric'],
         ];
     }
 }
