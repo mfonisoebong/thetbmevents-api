3e4494b3a5a88fe92ab95f6846c472fadaf507d7
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2025-12-28 01:47:24 +0100

    feat: add command to send sample email for SMTP testing

---
+++ resources/views/emails/sample.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>SMTP Test Email</title>
+</head>
+<body>
+    <h1>SMTP Test Email</h1>
+    <p>{{ $body }}</p>
+    <p>This message is sent by the <strong>send:sample-email</strong> artisan command.</p>
+</body>
+</html>
+
