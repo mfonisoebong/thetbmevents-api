b85929cb69f9779226b721d0b488fdc2fe792471
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2026-01-21 15:19:59 +0100

    fix: restored mistakenly deleted transaction table

---
+++ database/migrations/2023_08_20_130243_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->uuid('id')
+                ->primary();
+            $table->unsignedBigInteger('customer_id');
+            $table->enum('payment_method', ['vella', 'paystack']);
+            $table
+                ->enum('payment_status', ['pending', 'success', 'failed', 'reversed'])
+                ->default('pending');
+            $table->json('cart_items');
+            $table->string('transaction_reference');
+            $table->timestamps();
+
+            $table->foreign('customer_id')
+                ->references('id')
+                ->on('customers')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
