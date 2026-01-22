bb31e4579c790c695f35eb04597656186f87ce70
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2026-01-22 17:19:06 +0100

    fix: temporarily added deprecated fields

--- app/Http/Resources/UsersResource.php
+++ app/Http/Resources/UsersResource.php
@@ -16,14 +16,18 @@ class UsersResource extends JsonResource
     {
 
         return [
-            'name'=> $this->full_name,
+            'full_name'=> $this->full_name,
             'id'=> $this->id,
             'email'=> $this->email,
             'avatar'=>  $this->avatar,
             'phone'=> '('.$this->phone_dial_code.') - '.$this->phone_number,
             'created_at'=> $this->created_at,
             'role'=> $this->role,
-            'account_status'=> $this->account_state
+            'account_status'=> $this->account_state,
+            // todo: remove temporary fields in the future
+            'admin_role' => $this->role === 'admin' ? 'super_admin' : null,
+            'first_name' => explode(' ', $this->full_name)[0] ?? null,
+            'last_name' => explode(' ', $this->full_name)[1] ?? null
         ];
     }
 }
