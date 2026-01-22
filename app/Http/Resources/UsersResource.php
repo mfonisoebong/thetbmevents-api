6657a58876f4f7fd32cd978020cc16dbe81bea00
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2026-01-22 17:40:21 +0100

    added missing fields

--- app/Http/Resources/UsersResource.php
+++ app/Http/Resources/UsersResource.php
@@ -16,15 +16,20 @@ class UsersResource extends JsonResource
     {
 
         return [
-            'full_name'=> $this->full_name,
             'id'=> $this->id,
+            'full_name'=> $this->full_name,
+            'business_name'=> $this->business_name,
+            'completed_profile'=> $this->completed_profile,
+            'avatar'=> $this->avatar,
+            'auth_provider'=> $this->auth_provider,
             'email'=> $this->email,
-            'avatar'=>  $this->avatar,
-            'phone'=> '('.$this->phone_dial_code.') - '.$this->phone_number,
-            'created_at'=> $this->created_at,
             'role'=> $this->role,
-            'account_status'=> $this->account_state,
+            'country'=> $this->country,
+            'phone_number'=> $this->phone_number,
+            'account_state'=> $this->account_state,
+            'created_at'=> $this->created_at,
             // todo: remove temporary fields in the future
+            'account_status'=> $this->account_state,
             'admin_role' => $this->role === 'admin' ? 'super_admin' : null,
             'first_name' => explode(' ', $this->full_name)[0] ?? null,
             'last_name' => explode(' ', $this->full_name)[1] ?? null
