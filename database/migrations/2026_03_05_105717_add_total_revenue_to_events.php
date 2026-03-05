0783027a7a80215f323b40a1b73e12028e2bc33e
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2026-03-05 11:28:38 +0100

    increased total_revenue decimal limit

--- database/migrations/2026_03_05_105717_add_total_revenue_to_events.php
+++ database/migrations/2026_03_05_105717_add_total_revenue_to_events.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->decimal('total_revenue')->default(-1)->after('currency');
+            $table->decimal('total_revenue', 15)->default(-1)->after('currency');
         });
     }
 
